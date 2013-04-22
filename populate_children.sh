b86654cee42a99652d641f58e586bd5f20b2672e
Evan Bollig <bollig@gmail.com>
2013-04-22 14:42:52 -0500

    added script to clone subdirs. No need for git submodules to muddy the waters

---
+++ populate_children.sh
@@ -0,0 +1,7 @@
+git clone -b cleanup_jcp_typeset ssh://efb06@pamd.sc.fsu.edu/~efb06/repos/paper1.git 
+git clone -b master ssh://efb06@pamd.sc.fsu.edu/~efb06/repos/paper2.git 
+git clone -b master ssh://efb06@pamd.sc.fsu.edu/~efb06/repos/locality_sensitive_hashing.git 
+git clone -b master ssh://efb06@pamd.sc.fsu.edu/~efb06/repos/prospectus.git
+git clone -b master ssh://efb06@pamd.sc.fsu.edu/~efb06/repos/spear_benchmarks.git
+git clone -b master git@github.com:bollig/fsu_2012.git
+git clone -b master git@github.com:bollig/keeneland_benchmarks.git
