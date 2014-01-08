f0f5e6be80906399f867f3dda8021c631815123b
Evan BOllig <bollig@gmail.com>
2014-01-08 14:04:26 -0600

    Added Intel

--- figures/spmv/plotRoofline_M2070_M2050.m
+++ figures/spmv/plotRoofline_M2070_M2050.m
@@ -5,12 +5,21 @@ GFLOPS = 515 %GFLOP/s (double)
 
 figure(1)
 plotRoofline(BW,GFLOPS, 'NVidia M2050, M2070')
-
+ 
+hold on
 
 %% K20
 
 BW = 208 %GBytes/s
 GFLOPS = 1170 %GFLOPs
 
-figure(2)
+%figure(2)
 plotRoofline(BW,GFLOPS, 'NVidia K20')
+hold off
+
+%SOURCE: http://download.intel.com/newsroom/kits/xeon/phi/pdfs/Intel-Xeon-Phi-Coprocessor_ProductBrief.pdf
+BW = 320 %GBytes/s
+GFLOPS = 1011 %GFLOPs
+
+figure(3)
+plotRoofline(BW,GFLOPS, 'Intel Xeon Phi 5110P (B1)')
